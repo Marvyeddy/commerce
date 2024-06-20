4a9f691075ae09be4bba3ca81dc495a7be231500
Marvyeddy <anyatonwumarvelous32@gmail.com>
2024-06-20 07:36:17 +0100

    initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Category from '@/components/Category';
 import Products from '@/components/Products';
 import Image from 'next/image';
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <div>
       <Products />
+      <Category />
     </div>
   );
 }
